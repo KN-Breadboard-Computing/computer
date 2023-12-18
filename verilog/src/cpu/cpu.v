e95ff4850ea0ebd22828959bce53133943b31277
dawiderter <dwalczak456@gmail.com>
2023-12-18 19:24:01 +0100

    Fixed commas

--- verilog/src/cpu/cpu.v
+++ verilog/src/cpu/cpu.v
@@ -10,18 +10,18 @@ module cpu (
 
     // PROGRAM COUNTER
     wire [7:0] inner_pc_out;
-    counter inner_pc (
+    counter inner_pc(
         .clk(clk & ~signals[`PC_TICK]),
         .write(~signals[`PC_LOAD]),
         .reset(~signals[`PC_RST]),
-        .countdown(0),
+        .countdown(1'b0),
         .in(bus),
-        .out(inner_pc_out),
+        .out(inner_pc_out)
     );
     tristate_buffer pc(
         .data_in(inner_pc_out),
         .enable(~signals[`PC_OUT]),
-        .data_out(bus),
+        .data_out(bus)
     );
 
     // ALU REGISTERS
@@ -31,13 +31,13 @@ module cpu (
         .clk(clk),
         .enable(signals[`REG_A_LOAD]),
         .data_in(bus),
-        .data_out(alu_in),
+        .data_out(alu_in)
     );
     register b_reg (
         .clk(clk),
         .enable(signals[`REG_B_LOAD]),
         .data_in(bus),
-        .data_out(alu_in),
+        .data_out(alu_in)
     );
     
 
